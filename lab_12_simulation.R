b88ca82fe0f4bfc1d3b67f0d5dc8409d5c9dfd88
dyuan0226 <33184700+dyuan0226@users.noreply.github.com>
2019-08-01 13:18:52 -0400

    Added model_select function

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -4,3 +4,14 @@ generate_data <- function(n, p){
   return(list(covariates=covariates, responses=responses))
 }
 
+model_select <- function(covariates, responses, cutoff){
+  first_lm <- lm(responses ~ covariates)
+  p_values <- summary(first_lm)$coefficients[,"Pr(>|t|)"][-1]
+  cov_to_keep <- which(p_values<=cutoff)
+  if(length(cov_to_keep) == 0) return(vector())
+  else{
+    second_lm <- lm(responses ~ covariates[,cov_to_keep])
+    return(summary(second_lm)$coefficients[,"Pr(>|t|)"])
+  }
+}
+
