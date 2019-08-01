7360b1dfb28bb1c17cac727d592f3f977c2f2674
dyuan0226 <33184700+dyuan0226@users.noreply.github.com>
2019-08-01 13:55:24 -0400

    Added code for run_simulation with some tests

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -15,3 +15,17 @@ model_select <- function(covariates, responses, cutoff){
   }
 }
 
+run_simulation <- function(n_trials, n, p, cutoff){
+  p_values <- vector(mode="numeric")
+  for (trial in 1:n_trials){
+    sim <- generate_data(n, p)
+    p_values <- c(p_values, model_select(sim$covariates, sim$responses, cutoff))
+  }
+  return(ggplot(mapping=aes(x=p_values)) + geom_histogram())
+}
+
+for (n in c(100, 1000, 10000)){
+  for (p in c(10, 20, 50)){
+    run_simulation(n_trials=1000, n=n, p=p, cutoff=0.05)
+  }
+}
